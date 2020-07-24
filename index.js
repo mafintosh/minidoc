aa451d99b165f4e87ec6de08f85b736134653146
Mathias Buus <mathiasbuus@gmail.com>
2020-07-24 15:02:52 +0200

    fix missing else

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ module.exports = function html (name, opts = {}, children = []) {
     if (name === 'class') {
       if (Array.isArray(v)) for (const c of v) element.classList.add(c)
       else element.classList.add(v)
+    } else {
+      if (typeof v === 'function') element.addEventListener(name.replace(/^on/, ''), v)
+      else element.setAttribute(name, v)
     }
-    if (typeof v === 'function') element.addEventListener(name.replace(/^on/, ''), v)
-    else element.setAttribute(name, v)
   }
   for (let child of children) {
     if (typeof child !== 'object' || !child) child = document.createTextNode('' + (child || ''))
